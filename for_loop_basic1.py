549f004afb5a3bce662a1a66998c3c8dbb2a0dab
Jacob Roe <jroenavy24@gmail.com>
2021-05-03 16:39:57 -0700

    new commit

---
+++ for_loop_basic1.py
@@ -0,0 +1,30 @@
+# for x in range(0, 151 , 1):
+#     print(x)
+
+# for x in range(5, 1001, 5):
+#     print(x)
+
+# for x in range(0, 101 , 1):
+#     if x % 10 == 0:
+#         print("Coding Dojo")
+#     elif x % 5 == 0:
+#         print("Coding")
+#     else:
+#         print(x)
+
+# sum=0
+# for x in range(1, 500000, 2):
+#     sum = sum + x
+
+# print(sum)
+
+# for x in range(2018, 0, -4):
+#     print(x)
+
+lowNum = 2
+highNum = 20
+mult = 3
+
+for x in range(lowNum, highNum):
+    if x % mult == 0:
+        print(x)
