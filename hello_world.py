a867e1b6d6d49a8c63b79c5128a6bebc86ea069e
Jacob Roe <jroenavy24@gmail.com>
2021-05-03 14:24:51 -0700

    new commit

---
+++ hello_world.py
@@ -0,0 +1,21 @@
+# print("Hello World")
+
+# name="Jacob"
+# print("Hello " + name + "!")
+
+# name = "Jacob"
+# print("Hello", name , "!")
+
+# number = 24
+# print("Hello", number , "!")
+
+# number = 24
+# print("Hello "+ number + "!")
+
+# fav_food1 = "tacos"
+# fav_food2 = "pizza"
+# print("I love to eat {} and {}.".format(fav_food1, fav_food2))
+
+fav_food1 = "tacos"
+fav_food2 = "pizza"
+print(f"I love to eat {fav_food1} and {fav_food2}.")
