2590962149830124c2fa7c251421d7af87ffcf3f
Jacob Roe <jroenavy24@gmail.com>
2021-05-04 09:01:00 -0700

    commit

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,9 @@
 import random
 
+# computer_wins = 0
+# user_wins = 0
+
+# while (computer_wins <3 or player_wins < 3):
 user_input = input("Choose your weapon (rock, paper, scissors): ")
 weapon = ["rock", "paper", "scissors"]
 computer_input = random.choice(weapon)
@@ -10,15 +14,21 @@ if user_input == computer_input:
 elif user_input == "paper":
     if computer_input == "rock":
         print("Paper covers rock! You win!")
+        # user_wins += 1
     else:
         print("Scissors cuts paper! You lose.")
+        # computer_wins =+ 1
 elif user_input == "rock":
     if computer_input == "scissors":
         print("Rock crushes scissors! You win!")
+        # user_wins =+ 1
     else:
         print("Paper covers rock! You lose.")
+        # computer_wins =+ 1
 elif user_input == "scissors":
     if computer_input == "paper":
         print("Scissors cuts paper! You win!")
+        # user_wins =+ 1
     else:
         print("Rock smashes scissors! You lose.")
+        # computer_wins =+ 1
